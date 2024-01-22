9c758917530d8d9ef84670df7aa4c0fc8b2acb68
SlavaNekrish <vnekrish@mail.ru>
2024-01-22 21:02:41 +0300

    feat: implement 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout, stderr } = process;
+
+const absolutePath = path.join(__dirname, 'destination.txt');
+const output = fs.createWriteStream(absolutePath);
+
+stdout.write('Hello, my friend! Write something\n');
+
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    process.exit();
+  } else {
+    output.write(data);
+  }
+});
+
+process.on('exit', (code) => {
+  if (code === 0) {
+    stdout.write('Good luck, my friend!');
+  } else {
+    stderr.write(`Something went wrong. The program exited with code ${code}`);
+  }
+});
+
+process.on('SIGINT', () => {
+  console.log('Good luck, my friend!');
+  process.exit();
+});

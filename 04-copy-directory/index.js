a0e20da9f4fa3398537b7d77618994418f49b80d
SlavaNekrish <vnekrish@mail.ru>
2024-01-24 22:50:05 +0300

    feat: implement 04-copy-directory task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const absolutePath = path.join(__dirname, 'copy');
+
+fs.mkdir(absolutePath, () => {
+  fs.readdir(path.join(__dirname, 'copy'), (error, files) => {
+    if (error) throw error;
+    files.forEach((element) => {
+      fs.unlink(path.join(__dirname, 'copy', element), (error) => {
+        if (error) throw error;
+      });
+    });
+  });
+  fs.readdir(path.join(__dirname, 'files'), (error, files) => {
+    if (error) throw error;
+    files.forEach((element) => {
+      fs.copyFile(
+        path.join(__dirname, 'files', element),
+        path.join(__dirname, 'copy', element),
+        () => {},
+      );
+    });
+  });
+});
